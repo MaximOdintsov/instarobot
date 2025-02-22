e4120fb71563a2925211c163c5ed6efd862019a7
maxim <maxim.odintsov03@gmail.com>
2025-02-22 19:00:24 +0300

    bug fix

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@
 import click
 import os
 import importlib
-
 import asyncio
+from pathlib import Path
+
 from robot.database.orm import create_tables, async_engine
 from robot.conf import settings
 
@@ -13,19 +14,24 @@ def load_commands(cli):
     """
     Автоматически загружает все команды из папки config.COMMANDS_ROOT.
     """
-    for filename in os.listdir(settings.COMMANDS_ROOT):
-        if filename.endswith('.py') and filename != '__init__.py':
-            module_name = filename[:-3]
-            module_path = f'robot.commands.{module_name}'
-            try:
-                module = importlib.import_module(module_path)
-                # Ищем объекты, являющиеся командами (или группами) Click
-                for attr_name in dir(module):
-                    obj = getattr(module, attr_name)
-                    if isinstance(obj, click.Command):
-                        cli.add_command(obj)
-            except Exception as e:
-                click.echo(f"Не удалось загрузить команду из {module_name}: {e}", err=True)
+    root_path = Path(settings.COMMANDS_ROOT)
+    path_list = root_path.rglob('*.py')
+    for path in path_list:
+        if path.name == '__init__.py':
+            continue
+        module_name = path.stem
+        parent = path.parent.as_posix().replace("/", ".")
+        module_path = f'{parent}.{module_name}'
+
+        try:
+            module = importlib.import_module(module_path)
+            # Ищем объекты, являющиеся командами (или группами) Click
+            for attr_name in dir(module):
+                obj = getattr(module, attr_name)
+                if isinstance(obj, click.Command):
+                    cli.add_command(obj)
+        except Exception as e:
+            click.echo(f"Не удалось загрузить команду из {module_name}: {e}", err=True)
 
 
 @click.group()
